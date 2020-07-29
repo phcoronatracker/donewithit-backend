6c0369e0db11450782055c6d811060b694eb2fd7
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-07-29 22:55:15 +0800

    Procfile

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('trust proxy', 1);
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(session({
     secret: process.env.SECRET,
     resave: false,

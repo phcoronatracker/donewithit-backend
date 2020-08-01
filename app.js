39119c52456d77c37b8a9a6c1b141b07af79957e
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-02 04:54:51 +0800

    Separate mongodb config file

--- app.js
+++ app.js
@@ -1,105 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const findOrCreate = require("mongoose-findorcreate");
 const argon2 = require("./src/password");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const createThumbnail = require('./src/thumbnail');
+const model = require('./database/model');
 
 const app = express();
 const port = process.env.PORT || 9000;
+const { User, Listing, Image } = model;
 
-app.set('trust proxy', 1);
 app.use(bodyParser.json());
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false },
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-mongoose.connect(process.env.DB_URL, { dbName: "donewithit", useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }).then(() => {
-    console.log("Connected to Atlas Cluster successfully!");
-}).catch((err) => console.log(err));
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: {
-        type: String,
-        minlength: 8
-    },
-}, { versionKey: false });
-
-const imageSchema = new mongoose.Schema({
-    url: String,
-    thumbnail: String
-}, { versionKey: false, _id: false });
-
-const listingSchema = new mongoose.Schema({
-    title: String,
-    images: [imageSchema],
-    price: Number,
-    categoryId: Number,
-    userId: Number,
-    description: String,
-    location: {
-        latitude: mongoose.Types.Decimal128,
-        longitude: mongoose.Types.Decimal128,
-    }
-}, { versionKey: false })
-
-const encKey = process.env.ENC_KEY; //32-byte length 64-bit characters
-const sigKey = process.env.SIG_KEY; //64-byte length 64-bit characters
-
-userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields:['password'] });
-//userSchema.plugin(findOrCreate);
-
-const User = mongoose.model('User', userSchema);
-const Listing = mongoose.model('Listing', listingSchema);
-const Image = mongoose.model('Image', imageSchema);
-
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser((user, done) => {
-    done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
-});
-
-// use static authenticate method of model in LocalStrategy
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({ email: username }, function (err, user) {
-            if(err) { 
-                console.log("Error:", err);
-                return done(err); 
-            }
-            else if(!user) { 
-                console.log("No user");
-                return done(null, false); 
-            }
-            else {
-                argon2.password_verify(user.password, password).then(match => {
-                    if(!match) { 
-                        console.log("Password not match")
-                        return done(null, false);  
-                    }
-                    console.log("Password match");
-                    return done(null, user);
-                });
-            }
-        });
-    }
-));
 
 app.get('/', (req, res) => {
     res.send("Hello");
@@ -112,24 +22,9 @@ app.get('/listings', (req, res) => {
     });
 });
 
-
-app.post('/login', (req, res) => {
+app.post('/auth', (req, res) => {
     console.log(req.body);
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-
-    req.login(user, err => {
-        if(err) throw err;
-        console.log("No error");
-        //Creates a local cookie
-        passport.authenticate('local')(req, res, () => {
-            console.log("Auth Success!");
-            res.redirect('/');
-        });
-    });
-})
+});
 
 app.post('/register', (req, res) => {
     console.log(req.body);
@@ -176,7 +71,6 @@ app.post('/listings', async (req, res) => {
         console.log("Successfully added listing", listing._id);
         res.redirect('/');
     });
-
 });
 
 app.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
---
+++ database/model.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+mongoose.connect(process.env.DB_URL, { dbName: "donewithit", useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }).then(() => {
+    console.log("Connected to Atlas Cluster successfully!");
+}).catch((err) => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: {
+        type: String,
+        minlength: 8
+    },
+}, { versionKey: false });
+
+const imageSchema = new mongoose.Schema({
+    url: String,
+    thumbnail: String
+}, { versionKey: false, _id: false });
+
+const listingSchema = new mongoose.Schema({
+    title: String,
+    images: [imageSchema],
+    price: Number,
+    categoryId: Number,
+    userId: Number,
+    description: String,
+    location: {
+        latitude: mongoose.Types.Decimal128,
+        longitude: mongoose.Types.Decimal128,
+    }
+}, { versionKey: false })
+
+const encKey = process.env.ENC_KEY; //32-byte length 64-bit characters
+const sigKey = process.env.SIG_KEY; //64-byte length 64-bit characters
+
+userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields:['password'] });
+
+const User = mongoose.model('User', userSchema);
+const Listing = mongoose.model('Listing', listingSchema);
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = { User, Listing, Image }

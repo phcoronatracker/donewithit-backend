4a52bd155c9e6ed583841292a49251d7cb2a7d38
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-06 01:29:59 +0800

    Fixed bug of expo push token

--- routes/expoPushToken.js
+++ routes/expoPushToken.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const bodyParser = require("body-parser");
 const auth = require("../middleware/auth");
 const { User } = require("../database/model");
 
-router.use(bodyParser.json());
-
 router.post('/', auth, (req, res) => {
-    console.log(req.user);
     User.findById(req.user.userId, (err, docs) => {
         if(err) throw err;
         if(!docs) return res.status(422).send({ error: "User does not exist" });
 
         docs.expoPushToken = req.body.token;
-        console.log(req.body.token);
+        await docs.save();
         res.status(201).send("Sucessfully added push token");
     });
 });

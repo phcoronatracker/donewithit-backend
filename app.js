00d2c56b1ef0a1c9310827bc0dcac943d45508ca
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-06 01:34:09 +0800

    Fixed bug of expo push token

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require("express");
+const bodyParser = require('body-parser');
 const auth = require('./routes/auth');
 const listings = require('./routes/listings');
 const expoToken = require("./routes/expoPushToken");
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+app.use(bodyParser.json());
 
 app.use('/auth', auth);
 app.use('/listings', listings);

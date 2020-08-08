a58295015e4f2d137a5d19e7d16e5bae41b757cd
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-08 21:25:53 +0800

    Added socket io

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require('body-parser');
+const { Server } = require("http");
+const SocketIO = require('socket.io');
 const auth = require('./routes/auth');
 const listings = require('./routes/listings');
 const expoToken = require("./routes/expoPushToken");
@@ -8,6 +10,9 @@ const messages = require('./routes/messages');
 const upload = require("./routes/upload");
 
 const app = express();
+const server = Server(app);
+const io = SocketIO(server);
+
 const port = process.env.PORT || 9000;
 
 app.use(bodyParser.json());
@@ -18,4 +23,8 @@ app.use('/expo-push-token', expoToken);
 app.use('/messages', messages);
 app.use('/upload', upload);
 
-app.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
+io.on('connection', (socket) => {
+    console.log("A user connected:", socket);
+});
+
+server.listen(port, () => console.log(`App is listening on http://localhost:${port}`));

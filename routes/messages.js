2bb523d90a42e0d1d3b21fe8e1e38e0f5b01432d
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-10 04:46:33 +0800

    Configure socket

--- routes/messages.js
+++ routes/messages.js
@@ -52,7 +52,7 @@ router.get('/real-time', (req, res) => {
             if(err) throw err;
             if(!docs) return res.send("No Messages");
     
-            socket.to(socket.id).emit("messages", docs);
+            socket.emit("messages", docs);
         });
         // const data = verify(token);
 

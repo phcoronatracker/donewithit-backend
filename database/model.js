2ff98b6ddd50151eb7fd86311c0a67a7cdbad184
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-08-18 03:24:30 +0800

    Debugging socket message

--- database/model.js
+++ database/model.js
@@ -24,7 +24,7 @@ const messageSchema = new mongoose.Schema({
         image: String
     },
     image: String
-}, { versionKey: false });
+}, { versionKey: false, _id: String });
 
 const imageSchema = new mongoose.Schema({
     url: String,
